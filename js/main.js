a8e7da8c16bc3eba68e22fa56fa821ff224de552
zarazakill <blop22@yandex.ru>
2025-04-09 23:13:47 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { RadioPlayer } from './js/player/player.js';
+import { RadioPlayer } from './player/player.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     // Инициализация элементов управления

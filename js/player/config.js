a0a42bbbea86d99212d20ef361d6d2d05de830d3
zarazakill <blop22@yandex.ru>
2025-04-09 10:35:21 +0700

    Update config.js

--- js/player/config.js
+++ js/player/config.js
@@ -1,18 +1,10 @@
 export const RadioConfig = {
     streams: [
         { url: "https://wwcat.duckdns.org:8443/listen/algoritm-stream/radio", priority: 1 },
-        { url: "https://wwcat.duckdns.org:8000/radio", priority: 2 },
-        { url: "http://wwcat.hopto.org:8000/radio", priority: 3 }
+        { url: "https://wwcat.duckdns.org:8000/radio", priority: 2 }
     ],
     apiEndpoints: [
         "https://wwcat.duckdns.org:8443/api/nowplaying/1"
     ],
-    updateInterval: 10000,
-    reconnectDelay: 3000,
-    networkCheckInterval: 10000,
-    bufferLength: 20,
-    diagnostics: {
-        enabled: true,
-        logInterval: 60000
-    }
+    updateInterval: 10000
 };

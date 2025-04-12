681edf433d7df93eb1a5108d2217362758886030
zarazakill <blop22@yandex.ru>
2025-04-12 14:40:39 +0700

    Update player.js

--- js/player/player.js
+++ js/player/player.js
@@ -5,7 +5,6 @@ import { AudioController } from './audio-controller.js';
 
 export class RadioPlayer {
     constructor() {
-        const worker = new Worker('./data-worker.js');
         const requiredElements = ['stream-status', 'volume-slider', 'volume-btn'];
         for (const id of requiredElements) {
             if (!document.getElementById(id)) {

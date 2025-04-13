0a60ffd2fd1df31fcac2f1557518a5e4592c6cd3
zarazakill <blop22@yandex.ru>
2025-04-13 09:41:15 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { RadioPlayer } from './player/player.js';
+
 document.addEventListener('DOMContentLoaded', async () => {
     const playButton = document.getElementById('start-playback');
     const buttonText = playButton.querySelector('.button-text');

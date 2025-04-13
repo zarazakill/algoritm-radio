1f72742d9a1077d6101f9c1903a3adbb2edcf1ec
zarazakill <blop22@yandex.ru>
2025-04-13 08:30:20 +0700

    Update config.js

--- js/player/config.js
+++ js/player/config.js
@@ -19,8 +19,8 @@ const RadioPlayerConfig = {
         maxItems: 5,          // Максимальное количество отображаемых треков
         cacheSize: 20,        // Сколько треков хранить в кеше
         animationDelay: 100    // Задержка анимации между элементами (мс)
-    },
-        DEFAULT_THEME: 'dark'
+    },  // <- Добавлена запятая здесь
+    DEFAULT_THEME: 'dark'
 };
 
 // Экспорт конфигурации
